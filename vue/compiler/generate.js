7761eb945656cc19ef78755bc16e1921f8f4bfab
hgf <1454556135@qq.com>
2021-02-07 11:02:11 +0800

    新增模版编译模块

--- vue/compiler/generate.js
+++ vue/compiler/generate.js
@@ -1,6 +1,5 @@
-
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 function formatProps(attrs){
-    console.log(attrs)
     let attrStr = '';
     for(let i=0; i<attrs.length; i++){
         let attr = attrs[i];
@@ -12,19 +11,59 @@ function formatProps(attrs){
             })
             attr.value = styleAttrs;
         }
-        attrStr += `${attr.name}:${JSON.stringify(attr.value)}`
+        attrStr += `${attr.name}:${JSON.stringify(attr.value)},`
     }
-    console.log(`{${attrStr.slice(0, -1)}}`)
     return `{${attrStr.slice(0, -1)}}`
 }
 
+function generateChildren(node){
+    if(node.type === 1){
+        return generate(node)
+    }else if( node.type === 3){
+        let text = node.text;
+        //纯文本
+        if(!defaultTagRE.test(text)){
+            return `_v(${JSON.stringify(text)})`
+        }
+        let match,
+            index,
+            lastIndex = defaultTagRE.lastIndex = 0,
+            textArr = [];
+        while( match = defaultTagRE.exec(text) ){
+            index = match.index;
+            if(index > lastIndex){
+                textArr.push(JSON.stringify(text.slice(lastIndex,index)));
+            }
+            textArr.push(`_s(${match[1].trim()})`);
+            lastIndex = index + match[0].length
+        }
+        //匹配完{{}}长度还小于text证明后面还有纯文字文本
+        if(lastIndex < text.length){
+            textArr.push(JSON.stringify(text.slice(lastIndex)))
+        }
+
+        return  `_v(${textArr.join('+')})`
+    }
+}
+
+function getChildren(el){
+    const children = el.children;
+    if(children){
+        return children.map( c => generateChildren(c)).join(',');
+    }
+}
 
 function generate(el){
+    let children = getChildren(el);
     let code = `
-        _c('${el.tag}',{
-            ${el.attrs.length > 0 ?  formatProps(el.attrs) : 'undefind'}
+        _c('${el.tag}', ${
+            el.attrs.length > 0 ?  formatProps(el.attrs) : 'undefind'
+        }
+        ,${
+            children
         })
-    `
+    `  
+    return code;
 }
 
 export {
--- vue/compiler/index.js
+++ vue/compiler/index.js
@@ -2,9 +2,12 @@ import { parseHtmlToAst } from './astParser.js'
 import { generate } from './generate.js'
 
 function compileToRenderFunction(html){
-    const ast = parseHtmlToAst(html);
-    const code = generate(ast);
-    // console.log(code)
+    const ast = parseHtmlToAst(html),
+          code = generate(ast),
+          render = new Function(`
+            with(this) { return ${code} }
+          `);
+    console.log(render)
 }
 
 export {
